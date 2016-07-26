89d606160ccbf1503c8a1998dd2ee1eedc38a966
Ben Drucker <bvdrucker@gmail.com>
2016-07-25 22:38:26 -0700

    Add docs to cli

--- cli.js
+++ cli.js
@@ -9,6 +9,33 @@ const flatten = require('flatten')
 
 const nyc = path.resolve(child.execFileSync('npm', ['bin']).toString().trim(), 'nyc')
 const types = ['lines', 'functions', 'branches']
+const args = process.argv.slice(2)
+
+if (!args.length) {
+  const pkg = require('./package.json')
+  return console.log(`
+    ${pkg.description}
+
+    Usage
+      $ 100 <command> <args...>
+    Examples:
+      $ 100 tape test.js
+      $ 100 mocha test.js
+      $ 100 node test.js
+    Options:
+      https://github.com/istanbuljs/nyc
+
+
+    **   ****   ****
+   ***  *///** *///**
+  //** /*  */*/*  */*
+   /** /* * /*/* * /*
+   /** /**  /*/**  /*
+   /** /*   /*/*   /*
+   ****/ **** / ****
+  ////  ////   ////
+  `)
+}
 
 foreground(nyc, flatten([
   '--check-coverage',
